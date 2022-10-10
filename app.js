a07295de2b546c44d36708415ae5af54a8f142c7
king610160 <king610160@gmail.com>
2022-10-10 16:38:30 +0800

    feat: add sort function to arrange

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   Todo.find() //取出Todo model裡的所有資料
     .lean() //把Mongoose的Model物件轉換成乾淨的Javascript資料陣列
+    .sort({ _id:'asc' }) //desc
     .then(todos => res.render('index', { todos })) //將資料傳給index樣板
     .catch(error => console.log(error)) //錯誤處理
 })
